0eac0854b392006ff8309d9e3d60b7eefb94c8c1
Siwoo Lim <dlatldn426@gmail.com>
2023-09-03 17:51:39 +0900

    Size update

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 
 NOW=$(date "+Mirror-Disk-Total-Usage_%m-%d-%Y")
 df -BG | grep /siwoo.org > /siwoo.org-system/Mirror-disk-total-usage/$NOW
-cd /root/Mirror-disk-total-usage
-git add /root/Mirror-disk-total-usage/$NOW
+cd /siwoo.org-system/Mirror-disk-total-usage
+git add *
 git commit -m "Size update"
 git push origin master
